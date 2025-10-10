17708b5227eaf7a378a10be08b0223b985b3c71f
CPlusPlus129 <cmp8752@g.rit.edu>
2025-10-10 14:42:51 -0400

    Basic light system implementation

--- Cook Project/Assets/Scripts/System/PlayerStatSystem.cs
+++ Cook Project/Assets/Scripts/System/PlayerStatSystem.cs
@@ -7,6 +7,11 @@ public class PlayerStatSystem : SimpleSingleton<PlayerStatSystem>
     public ReactiveProperty<float> CurrentStamina { get; private set; } = new ReactiveProperty<float>(100);
     public ReactiveProperty<float> MaxStamina { get; private set; } = new ReactiveProperty<float>(100);
 
+    // Light System Properties
+    public ReactiveProperty<float> CurrentLight { get; private set; } = new ReactiveProperty<float>(100f);
+    public ReactiveProperty<float> MaxLight { get; private set; } = new ReactiveProperty<float>(100f);
+    public ReactiveProperty<float> LightRecoverySpeed { get; private set; } = new ReactiveProperty<float>(5f);
+
     public ReactiveProperty<int> Money { get; private set; } = new ReactiveProperty<int>(200);
 
     public ReactiveProperty<float> StaminaRecoverySpeed { get; private set; } = new ReactiveProperty<float>(10f);
