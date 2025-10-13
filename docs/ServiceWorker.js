911fc19a16d90c592795186e72d6c1b0f00d9fc8
Koa <kelen6107@gmail.com>
2025-10-13 14:05:57 -0400

    build update

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Cook Project-0.1.1";
+const cacheName = "DefaultCompany-Cook Project-0.1.2";
 const contentToCache = [
     "Build/docs.loader.js",
     "Build/docs.framework.js",
