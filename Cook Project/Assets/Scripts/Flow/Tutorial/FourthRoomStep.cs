0273070d896325943cda064eeab5a168b0552a92
Koa <kelen6107@gmail.com>
2025-10-21 17:11:00 -0400

    forgor to add these bois

---
+++ Cook Project/Assets/Scripts/Flow/Tutorial/FourthRoomStep.cs
@@ -0,0 +1,41 @@
+using Cysharp.Threading.Tasks;
+using R3;
+
+class FourthRoomStep : ITutorialStep
+{
+    private readonly IInventorySystem inventorySystem;
+    private readonly FoodSource foodSource;
+    private readonly SimpleDoor door;
+    private CompositeDisposable disposables = new CompositeDisposable();
+
+    public FourthRoomStep(IInventorySystem inventorySystem, FoodSource foodSource)
+    {
+        this.inventorySystem = inventorySystem;
+        this.foodSource = foodSource;
+    }
+
+    public async UniTask ExecuteAsync()
+    {
+        await WaitUntilPlayerGetsFoodFromSource();
+        //TODO arrow
+    }
+
+    private async UniTask WaitUntilPlayerGetsFoodFromSource()
+    {
+        var tcs = new UniTaskCompletionSource();
+        inventorySystem.OnInventoryChanged
+            .Subscribe(list =>
+            {
+                foreach (var item in list)
+                {
+                    if (item != null && item.ItemName == foodSource.ItemName)
+                    {
+                        tcs.TrySetResult();
+                        disposables.Clear();
+                    }
+                }
+            }).AddTo(disposables);
+
+        await tcs.Task;
+    }
+}
---
+++ Cook Project/Assets/Scripts/Flow/Tutorial/ThirdRoomStep.cs
@@ -0,0 +1,42 @@
+using Cysharp.Threading.Tasks;
+using R3;
+
+class ThirdRoomStep : ITutorialStep
+{
+    private readonly IInventorySystem inventorySystem;
+    private readonly FoodSource foodSource;
+    private readonly SimpleDoor door;
+    private CompositeDisposable disposables = new CompositeDisposable();
+
+    public ThirdRoomStep(IInventorySystem inventorySystem, FoodSource foodSource, SimpleDoor door)
+    {
+        this.inventorySystem = inventorySystem;
+        this.foodSource = foodSource;
+        this.door = door;
+    }
+
+    public async UniTask ExecuteAsync()
+    {
+        await WaitUntilPlayerGetsFoodFromSource();
+        door.Open();
+    }
+
+    private async UniTask WaitUntilPlayerGetsFoodFromSource()
+    {
+        var tcs = new UniTaskCompletionSource();
+        inventorySystem.OnInventoryChanged
+            .Subscribe(list =>
+            {
+                foreach (var item in list)
+                {
+                    if (item != null && item.ItemName == foodSource.ItemName)
+                    {
+                        tcs.TrySetResult();
+                        disposables.Clear();
+                    }
+                }
+            }).AddTo(disposables);
+
+        await tcs.Task;
+    }
+}
