890ed404004bd2e7b1875b13e21ac5354c66c71a
Koa <kelen6107@gmail.com>
2025-10-20 22:18:20 -0400

    make InventoryBarUI dynamic with slots

--- Cook Project/Assets/Scripts/UI/InventoryBarUI.cs
+++ Cook Project/Assets/Scripts/UI/InventoryBarUI.cs
@@ -1,29 +1,49 @@
-using UnityEngine;
 using R3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class InventoryBarUI : MonoBehaviour
 {
-    public InventorySlotUI[] slots;
-    public GameObject selectedIndicator;
+    [SerializeField] private GameObject selectedIndicator;
+    [SerializeField] private InventorySlotUI slotPrefab;
+    private List<InventorySlotUI> slots = new List<InventorySlotUI>();
 
     private void Awake()
     {
+        slotPrefab.gameObject.SetActive(false);
         InventorySystem.Instance.OnInventoryChanged.Subscribe(UpdateInventory).AddTo(this);
         InventorySystem.Instance.SelectedIndex.Subscribe(OnSelectedIndexChanged).AddTo(this);
         ClearItems();
+        UpdateInventory(InventorySystem.Instance.GetAllItems().ToArray());
+        OnSelectedIndexChanged(0);
     }
 
     private void UpdateInventory(ItemBase[] items)
     {
-        for (int i = 0; i < slots.Length; i++)
+        for (int i = 0; i < items.Length; i++)
         {
-            if (i < items.Length)
+            var item = items[i];
+            if (i < slots.Count)
             {
-                slots[i].SetItem(items[i]?.ItemName);
+                slots[i].SetItem(item?.ItemName);
             }
             else
             {
-                slots[i].SetItem(null);
+                var newSlot = Instantiate(slotPrefab, slotPrefab.transform.parent);
+                newSlot.gameObject.SetActive(true);
+                slots.Add(newSlot);
+                newSlot.SetItem(item?.ItemName);
+            }
+        }
+        if(slots.Count > items.Length)
+        {
+            for (int i = slots.Count - 1; i >= items.Length; i--)
+            {
+                var slot = slots[i];
+                slots.RemoveAt(i);
+                Destroy(slot);
             }
         }
     }
@@ -38,7 +58,7 @@ public class InventoryBarUI : MonoBehaviour
 
     private void OnSelectedIndexChanged(int index)
     {
-        if (index >= 0 && index < slots.Length)
+        if (index >= 0 && index < slots.Count)
         {
             selectedIndicator.transform.SetParent(slots[index].transform, false);
         }
