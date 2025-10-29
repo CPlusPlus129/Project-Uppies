9377b00c8160a5c394c48610d58c4eabba5a1b2e
CPlusPlus129 <cmp8752@g.rit.edu>
2025-10-29 14:40:34 -0400

    Fade other UI elements when dialogue playing

--- Cook Project/Assets/DialogueModule/Scripts/UI/DialogueUIManager.cs
+++ Cook Project/Assets/DialogueModule/Scripts/UI/DialogueUIManager.cs
@@ -9,9 +9,26 @@ namespace DialogueModule
         private List<MonoBehaviour> bindableSources = new List<MonoBehaviour>();
         [SerializeField]
         private DialogueEngine engine;
+        [SerializeField]
+        private List<CanvasGroup> layersToDim = new List<CanvasGroup>();
+        [SerializeField, Range(0f, 1f)]
+        private float dialogueOpacity = 0f;
+        [SerializeField]
+        private bool disableInteractionDuringDialogue = true;
+        [SerializeField]
+        private bool disableRaycastsDuringDialogue = true;
 
         private bool _isInited = false;
         private readonly List<IScenarioBindable> _bindables = new List<IScenarioBindable>();
+        private readonly Dictionary<CanvasGroup, LayerState> _layerStateCache = new Dictionary<CanvasGroup, LayerState>();
+        private bool _layersDimmed = false;
+
+        private struct LayerState
+        {
+            public float alpha;
+            public bool interactable;
+            public bool blocksRaycasts;
+        }
 
         protected void Awake()
         {
@@ -23,6 +40,7 @@ namespace DialogueModule
             engine.uiHasInit = false;
             UnbindSelf();
             Unbind();
+            RestoreLayerStates();
         }
 
         public void Init()
@@ -32,6 +50,7 @@ namespace DialogueModule
             SetEngine();
             BindSelf();
             CacheBindables();
+            CacheLayerStates();
             Bind();
             engine.uiHasInit = true;
         }
@@ -88,16 +107,19 @@ namespace DialogueModule
             {
                 bindables?.UnbindFromScenario(engine.adapter);
             }
+            RestoreLayerStates();
         }
 
         private void OnStartScenario()
         {
             gameObject.SetActive(true);
+            ApplyDialogueLayerStates();
         }
 
         private void OnEndScenario()
         {
             gameObject.SetActive(false);
+            RestoreLayerStates();
         }
 
         private void SetEngine()
@@ -113,6 +135,94 @@ namespace DialogueModule
         public void SetEngine(DialogueEngine engine)
         {
             this.engine = engine;
+        }
+
+        private void CacheLayerStates()
+        {
+            _layerStateCache.Clear();
+            foreach (var group in layersToDim)
+            {
+                if (group == null)
+                {
+                    continue;
+                }
+
+                if (_layerStateCache.ContainsKey(group))
+                {
+                    continue;
+                }
+
+                _layerStateCache[group] = new LayerState
+                {
+                    alpha = group.alpha,
+                    interactable = group.interactable,
+                    blocksRaycasts = group.blocksRaycasts
+                };
+            }
+        }
+
+        private void ApplyDialogueLayerStates()
+        {
+            if (_layersDimmed)
+            {
+                return;
+            }
+
+            foreach (var group in layersToDim)
+            {
+                if (group == null)
+                {
+                    continue;
+                }
+
+                if (!_layerStateCache.ContainsKey(group))
+                {
+                    _layerStateCache[group] = new LayerState
+                    {
+                        alpha = group.alpha,
+                        interactable = group.interactable,
+                        blocksRaycasts = group.blocksRaycasts
+                    };
+                }
+
+                group.alpha = dialogueOpacity;
+
+                if (disableInteractionDuringDialogue)
+                {
+                    group.interactable = false;
+                }
+
+                if (disableRaycastsDuringDialogue)
+                {
+                    group.blocksRaycasts = false;
+                }
+            }
+
+            _layersDimmed = true;
+        }
+
+        private void RestoreLayerStates()
+        {
+            if (!_layersDimmed)
+            {
+                return;
+            }
+
+            foreach (var kvp in _layerStateCache)
+            {
+                var group = kvp.Key;
+                var state = kvp.Value;
+                if (group == null)
+                {
+                    continue;
+                }
+
+                group.alpha = state.alpha;
+                group.interactable = state.interactable;
+                group.blocksRaycasts = state.blocksRaycasts;
+            }
+
+            _layersDimmed = false;
         }
     }
 }
