5eec30d25c651db45403916bf95e551559a8fa06
ViscLu <112127908+ViscLu@users.noreply.github.com>
2025-11-18 15:03:28 -0700

    Add fire pillar to temp level layout

---
+++ Cook Project/Assets/Scripts/FirePillarRotation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class FirePillarRotation : MonoBehaviour
+{
+    [Header("Rotate Speed")]
+    public float rotateSpeed = 30f;
+
+    bool isRotating = true;
+
+    void Update()
+    {
+        if (isRotating)
+        {
+            RotateOnce();
+        }
+    }
+
+    public void RotateOnce()
+    {
+        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+    }
+
+    public void StartRotate()
+    {
+        isRotating = true;
+    }
+
+    public void StopRotate()
+    {
+        isRotating = false;
+    }
+}
